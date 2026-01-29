2e5b2a31c6cbb77783950998c78a7a090c853100
Honzha Yehor <honzhayehor@gmail.com>
2026-01-29 13:11:58 +0200

    Basic Player class implemented

--- src/main/java/logic/Player.java
+++ src/main/java/logic/Player.java
@@ -1,4 +1,24 @@
 package logic;
 
 public class Player {
+
+    private int attempts = 0;
+    private boolean isAlive = true;
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public boolean isAlive() {
+        return isAlive;
+    }
+
+    public void setAlive(boolean alive) {
+        isAlive = alive;
+    }
+
 }
